a78c0c4fdbbf656f3aa008cccd3305d47fe57c46
Roman Kulish <roman.kulish@smashdocs.net>
2022-03-02 18:36:35 +0200

    1st

---
+++ virtual-1.tf
@@ -0,0 +1,31 @@
+resource "digitalocean_droplet" "virtual-1" {
+  image = "ubuntu-20-04-x64"
+  name = "virtual-1"
+  region = "ams3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install bombardier and start
+      "apt update",
+      "apt install -y git curl",
+      "snap install docker",
+      "git clone https://github.com/fms-ukraine/stop-war.git",
+      "docker run -it $(docker build -q /root/stop-war/) &",
+      "docker run -it $(docker build -q /root/stop-war/) &",
+      "docker run -it $(docker build -q /root/stop-war/) &"
+    ]
+  }
+}
